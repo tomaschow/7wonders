e24cf7608ff9fb79318890b81c973b2619c4da4a
Dingda Zhou <tomaschow95@outlook.com>
2022-10-03 15:46:31 -0400

    linter stuff

--- .eslintrc.js
+++
@@ -1,7 +0,0 @@
-module.exports = {
-    extends: "tomi-eslint-config",
-    globals: {
-      // we can use types like JSX.Element without upsetting eslint
-      JSX: true
-    }
-  };
---
+++ CHANGELOG.md
@@ -0,0 +1,5 @@
+## 0.1.1
+- Linter cleanup
+
+## 0.1.0
+- Initial commit
